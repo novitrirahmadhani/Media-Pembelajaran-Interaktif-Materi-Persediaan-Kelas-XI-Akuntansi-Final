481d9e11edb10e1b833ebd92679c87ba7e57fba5
novitrirahmadhani <novitrirahmadhani@gmail.com>
2026-05-02 23:46:26 +0700

    bismillah dipermudah

--- story_content/user.js
+++ story_content/user.js
@@ -2,18 +2,6 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "6AIV1s43Vs3":
-        Script1();
-        break;
   }
-}
-
-function Script1()
-{
-  var audio = document.getElementById('bgSongku');
-audio.src="backsound.mp3";
-audio.load();
-audio.play();
-audio.volume=0.5
 }
 
